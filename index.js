d9a2c174677028fd6471652871012df5006b9147
Srujangupta06 <120577073+Srujangupta06@users.noreply.github.com>
2024-05-22 23:29:28 +0530

    Add files via upload
    Game has Related JavaScript concepts like math.random(), math.ceil(), conditionals, DOM manipulations

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-function switchOff() {
-    document.getElementById("bulbImage").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
-    document.getElementById("catImage").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
-    document.getElementById("switchStatus").textContent = "Switched Off";
-    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
-    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+function checkGuess() {
+  let guessedNumber = parseInt(userInput.value);
+  if (guessedNumber > randomNumber) {
+    gameResult.textContent = "Too High! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
   }
-  
-  function switchOn() {
-    document.getElementById("bulbImage").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
-    document.getElementById("catImage").src =
-      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
-    document.getElementById("switchStatus").textContent = "Switched On";
-    document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
-    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
+  else if (guessedNumber < randomNumber) {
+    gameResult.textContent = "Too Low! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
   }
+  else if (guessedNumber === randomNumber) {
+    gameResult.textContent = "Congratulations! You got it right.";
+    gameResult.style.backgroundColor = "green";
+  }
+  else {
+    gameResult.textContent = "Please provide a valid input.";
+    gameResult.style.backgroundColor = "red";
+  }
+}
